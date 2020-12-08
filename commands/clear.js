1f65621d54c76a808e214828a7490962dee8ce5e
Emil W <emilwarelius@gmail.com>
2020-12-08 12:49:21 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -9,5 +9,14 @@ module.exports = {
         if (!amount) return message.reply("You have to specify an amount of messages to delete.");
         if (isNaN(amount)) return message.reply('The amount parameter isn`t a number!');
 
+        if (amount > 100) return message.reply("You cannot clear more than 100 messages at once");
+        if (amount < 1) return message.reply("You must specify an amount between 1-100");
+
+        message.channel.messages.fetch((limit: amount)).then(messages => {
+            message.channel.bulkDelete(messages)
+        });
+
+        message.channel.send('\` Deleted' + amount + ' message\´');
+
     }
 }
